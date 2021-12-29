eedd0d9cc31695d0ec0a29f77739d4cd978c3982
ArianNexux <ariannexux0101@gmail.com>
2021-12-30 00:49:00 +0100

    creaet first use case

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,6 +1,23 @@
-import {Entity} from "typeorm";
+import {Column, Entity, PrimaryColumn, UpdateDateColumn, CreateDateColumn} from "typeorm";
 
-@Entity()
+@Entity("users")
 export class User {
 
+    @PrimaryColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    admin: boolean
+
+    @CreateDateColumn()
+    created_at:Date
+
+    @UpdateDateColumn()
+    updated_at:Date
 }
---
+++ backend/src/repositories/UserRepositories.ts
@@ -0,0 +1,11 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { User } from '../entities/User'
+
+@EntityRepository(User)
+class UserRepositories extends Repository<User> {
+
+
+
+} 
+
+export { UserRepositories }
