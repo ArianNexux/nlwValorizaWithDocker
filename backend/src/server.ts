bdf5022628a75d0e6df44c4b5922354aac8f6f75
ArianNexux <ariannexux0101@gmail.com>
2021-12-31 23:53:24 +0100

    feat: add middleware for exceptions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,14 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
+app.use((err, request, response, next)=>{
+    if(err instanceof Error){
+        return response.status(400).json({ message: err.message})
+    }
+
+    return response.status(500).json({ message: 'Internal server error'})
+})
+
 app.listen(3000, ()=>{
     console.log("server running")
 })
