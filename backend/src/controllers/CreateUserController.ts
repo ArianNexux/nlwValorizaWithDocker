fb46c5fa102b8bda919ecb70f51701449895050d
ArianNexux <ariannexux0101@gmail.com>
2021-12-31 23:34:46 +0100

    refactor: treating exception on CreateUserController

--- backend/src/controllers/CreateUserController.ts
+++ backend/src/controllers/CreateUserController.ts
@@ -4,14 +4,20 @@ import IUseCase from '../interfaces/IUseCase'
 import { CreateUser } from '../usecases/CreateUser'
 class CreateUserController {
 
+    
     async handle(request: Request, response: Response): Promise<Response>{
-        const { name, email, admin } = request.body;
+        try{
+            const { name, email, admin } = request.body;
+            
+            const createUserService = new CreateUser();
+            const user = await createUserService.handle({name, email, admin})
+
+            return response.json(user)
+
         
-        const createUserService = new CreateUser();
-        const user = await createUserService.handle({name, email, admin})
-
-        return response.json(user)
-
+        } catch(err){
+            return response.status(400).json({message:err.message})
+        }
     }
 
 }
