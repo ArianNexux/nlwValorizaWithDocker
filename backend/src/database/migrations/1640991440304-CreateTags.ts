d5ea8aa19efb20d9c8859e3d788550552e0232fb
ArianNexux <ariannexux0101@gmail.com>
2021-12-31 23:58:17 +0100

    feat: add tags migration

---
+++ backend/src/database/migrations/1640991440304-CreateTags.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTags1640991440304 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
