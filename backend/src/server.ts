3e9d03fd594949af369921072ce4094ce2717f61
ArianNexux <ariannexux0101@gmail.com>
2022-01-08 17:22:27 +0100

    refactor: typing next param

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
 import './database'
 import 'express-async-errors'
-import express,{ Response, Request} from 'express'
+import express,{ Response, Request, NextFunction} from 'express'
 import router from './routes'
 
 const app = express()
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
-app.use((err: Error, request: Request, response: Response, next)=>{
+app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
     if(err instanceof Error){
         return response.status(400).json({ message: err.message})
     }
